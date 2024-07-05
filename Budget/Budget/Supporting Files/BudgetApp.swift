48f01ecf1b721b9c4101e94335958e7d469d3b92
Sukhaman Singh <sukhamandev@gmail.com>
2024-07-05 09:03:51 -0400

    Firebase library added

---
+++ Budget/Budget/Supporting Files/BudgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  BudgetApp.swift
+//  Budget
+//
+//  Created by Sukhaman Singh on 7/5/24.
+//
+
+import SwiftUI
+
+@main
+struct BudgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
